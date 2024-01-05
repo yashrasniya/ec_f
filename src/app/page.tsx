978f72caf98ea6ebba576aad70c301505e830b83
Yash <yashrasniya3@gmail.com>
2024-01-05 20:32:09 +0530

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,89 @@
-import Image from 'next/image'
+
+import React from "react";
+import {Nav_bar} from "@/components/navbar";
+import {Search_bar} from "@/components/search";
+import {Carousel2} from "@/app/home/Carousel";
+
+
+
+const Product_preview = (e: { img: string; price: string; title: string; live: boolean }) => {
+  return(
+      <div className={'bg-[#fff] w-full text-[10px]  p-3 rounded-md'}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={e?.img}  className={'w-full'} alt={''} />
+        <p>{e?.title}</p>
+        <p className={'flex gap-2'}>₹{e?.price}/-
+          {e?.live?<svg xmlns="http://www.w3.org/2000/svg" width="11" height="7" viewBox="0 0 11 7" fill="none">
+            <path
+                d="M10.2458 4.5358L5.9558 0.295798C5.86283 0.20207 5.75223 0.127676 5.63037 0.0769072C5.50852 0.0261385 5.37781 0 5.2458 0C5.11379 0 4.98308 0.0261385 4.86122 0.0769072C4.73936 0.127676 4.62876 0.20207 4.5358 0.295798L0.295798 4.5358C0.20207 4.62876 0.127675 4.73936 0.0769067 4.86122C0.026138 4.98308 0 5.11379 0 5.2458C0 5.37781 0.026138 5.50852 0.0769067 5.63037C0.127675 5.75223 0.20207 5.86283 0.295798 5.9558C0.483161 6.14205 0.736612 6.24659 1.0008 6.24659C1.26498 6.24659 1.51844 6.14205 1.7058 5.9558L5.2458 2.4158L8.7858 5.9558C8.97206 6.14054 9.22346 6.24469 9.4858 6.2458C9.6174 6.24656 9.74787 6.22133 9.86971 6.17157C9.99154 6.12181 10.1024 6.04848 10.1958 5.9558C10.2929 5.86618 10.3712 5.75821 10.4263 5.63814C10.4814 5.51806 10.5122 5.38826 10.5169 5.25622C10.5215 5.12419 10.4999 4.99254 10.4534 4.86889C10.4069 4.74523 10.3363 4.63202 10.2458 4.5358Z"
+                fill="#01FF02"/>
+          </svg>:''}
+        </p>
+      </div>
+  )
+}
+const Live_Bargaining = () => {
+  let live=[
+    {title:'yash',img:'/01.png',price:'25,000',live:true},
+    {title:'yash',img:'/01.png',price:'25,000',live:true},
+    {title:'yash',img:'/01.png',price:'25,000',live:true},
+
+  ]
+  return(
+      <div className={'w-full text-primary-1'}>
+        <h3 className={' font-medium mt-2 '}>
+          Live Bargaining
+        </h3>
+        <div className={'grid grid-cols-2  gap-2 pt-2'}>
+          {
+            live.map(Product_preview)
+          }
+
+        </div>
+
+      </div>
+  )
+}
+const Recommender_product = () => {
+  let live=[
+    {title:'yash',img:'/01.png',price:'25,000',live:false},
+    {title:'yash',img:'/01.png',price:'25,000',live:false},
+    {title  :'yash',img:'/01.png',price:'25,000',live:false},
+    {title  :'yash',img:'/01.png',price:'25,000',live:false},
+    {title  :'yash',img:'/01.png',price:'25,000',live:false},
+    {title  :'yash',img:'/01.png',price:'25,000',live:false},
+    {title  :'yash',img:'/01.png',price:'25,000',live:false},
+  ]
+  return(
+      <div className={'w-full text-primary-1'}>
+        <h3 className={' font-medium mt-2 '}>
+          Recommender Product
+        </h3>
+        <div className={'grid grid-cols-2  gap-2 pt-2'}>
+          {
+            live.map(Product_preview)
+          }
+
+        </div>
+
+      </div>
+  )
+}
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <>
+        <div className={'root p-4 '}>
+          <Search_bar></Search_bar>
+          <Carousel2></Carousel2>
+          <Live_Bargaining></Live_Bargaining>
+          <Recommender_product></Recommender_product>
+
         </div>
-      </div>
+        <Nav_bar></Nav_bar>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      </>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
 }
